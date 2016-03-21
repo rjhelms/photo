ea9d3f299957127625a04c5ccc3eaa60518f93a6
Rob Hailman <robhailman@gmail.com>
2016-03-21 12:15:04 -0400

    Add basics models for film

--- src/photo/admin.py
+++ src/photo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from photo.models import FilmFormat, Manufacturer, Film
+
+
 # Register your models here.
+admin.site.register(FilmFormat)
+admin.site.register(Manufacturer)
+admin.site.register(Film)
--- src/photo/models.py
+++ src/photo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class FilmFormat(models.Model):
+    name = models.CharField(max_length=50)
+    roll_film = models.BooleanField()
+    
+    def __str__(self):
+        return self.name
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=50)
+    short_name = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+    
+class Film(models.Model):
+    name = models.CharField(max_length=50)
+    manufacturer = models.ForeignKey(Manufacturer)
+    speed = models.IntegerField()
+    formats = models.ManyToManyField(FilmFormat)
+    
+    def __str__(self):
+        return "{} {}".format(self.manufacturer.short_name, self.name)
+    
