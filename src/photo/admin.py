2006208753eb1e79940cca40dd7f5301ae5f406c
Rob Hailman <robhailman@gmail.com>
2016-03-21 13:39:50 -0400

    Add Developer and FilmRoll models
    Just more base models

--- src/photo/admin.py
+++ src/photo/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-from photo.models import FilmFormat, Manufacturer, Film
+from photo.models import FilmFormat, Manufacturer, Film, Developer, FilmRoll
 
+class FilmRollAdmin(admin.ModelAdmin):
+    list_display = ('name', 'film', 'format', 'shot_date',)
+    list_filter = ('film', 'format', 'shot_date', 'developed_date',)
+    prepopulated_fields = {'developed_speed': ('shot_speed',)}
+    
+    def get_changeform_initial_data(self, request):
+        initial = super().get_changeform_initial_data(request)
+        initial['photographer'] = request.user
+        return initial
 
 # Register your models here.
 admin.site.register(FilmFormat)
 admin.site.register(Manufacturer)
 admin.site.register(Film)
+admin.site.register(Developer)
+admin.site.register(FilmRoll, FilmRollAdmin)
--- src/photo/models.py
+++ src/photo/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
 class FilmFormat(models.Model):
     name = models.CharField(max_length=50)
     roll_film = models.BooleanField()
-    
+
     def __str__(self):
         return self.name
 
@@ -12,16 +13,41 @@ class FilmFormat(models.Model):
 class Manufacturer(models.Model):
     name = models.CharField(max_length=50)
     short_name = models.CharField(max_length=20)
-    
+
     def __str__(self):
         return self.name
-    
+
 class Film(models.Model):
     name = models.CharField(max_length=50)
     manufacturer = models.ForeignKey(Manufacturer)
     speed = models.IntegerField()
     formats = models.ManyToManyField(FilmFormat)
-    
+
     def __str__(self):
         return "{} {}".format(self.manufacturer.short_name, self.name)
+
+class Developer(models.Model):
+    name = models.CharField(max_length=50)
+    manufacturer = models.ForeignKey(Manufacturer)
+    powder = models.BooleanField()
+
+    def __str__(self):
+        return "{} {}".format(self.manufacturer.short_name, self.name)
+
+class FilmRoll(models.Model):
+    name = models.CharField(max_length=50, unique=True, db_index=True)
+    film = models.ForeignKey(Film)
+    format = models.ForeignKey(FilmFormat)
+    developer = models.ForeignKey(Developer, blank=True, null=True)
+    shot_speed = models.PositiveIntegerField()
+    developed_speed = models.PositiveIntegerField()
+    shot_date = models.DateField(blank=True, null=True)
+    developed_date = models.DateField(blank=True, null=True)
+    photographer = models.ForeignKey(auth.models.User, blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
+    def _base_speed(self):
+        return self.film.speed
     
