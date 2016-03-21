b5e574b026e789f74e24f207aa12c06fc9248d66
Rob Hailman <robhailman@gmail.com>
2016-03-21 15:03:28 -0400

    Improve admin UI for Film model
    Implement admin class for Film, with sorting and filtering.

--- src/photo/admin.py
+++ src/photo/admin.py
@@ -4,6 +4,24 @@ from photo.models import FilmFormat, Manufacturer, Film, Developer, FilmRoll, \
     PhotoPaper, PhotoPaperFinish
 
 
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ('film_short_name', 'manufacturer_short_name', 'speed',)
+    list_filter = ('manufacturer__name', 'speed', 'formats')
+
+    def film_short_name(self, obj):
+        return ("%s %s" % (obj.manufacturer.short_name, obj.name))
+
+    def manufacturer_short_name(self, obj):
+        return obj.manufacturer.short_name
+
+    film_short_name.admin_order_field = 'name'
+    film_short_name.short_description = "Film"
+
+    manufacturer_short_name.admin_order_field = 'manufacturer__short_name'
+    manufacturer_short_name.short_description = "Manufacturer"
+
+    ordering = ('manufacturer__short_name', 'name')
+
 class FilmRollAdmin(admin.ModelAdmin):
     list_display = ('name', 'film', 'format', 'shot_date',)
     list_filter = ('film', 'format', 'shot_date', 'developed_date',)
@@ -17,7 +35,7 @@ class FilmRollAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(FilmFormat)
 admin.site.register(Manufacturer)
-admin.site.register(Film)
+admin.site.register(Film, FilmAdmin)
 admin.site.register(Developer)
 admin.site.register(FilmRoll, FilmRollAdmin)
 admin.site.register(PhotoPaper)
