b1b399eb9b4602bef7d97009515bbc6e7a960006
Rob Hailman <robhailman@gmail.com>
2016-03-21 14:29:50 -0400

    Add models for photo paper
    Photo papers have finishes, so a supporting model for that as well. This
    could just as easily have been done by having finish as a choice field,
    but this solution supports different ranges of finishes across different
    paper manufacturers.

--- src/photo/admin.py
+++ src/photo/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from photo.models import FilmFormat, Manufacturer, Film, Developer, FilmRoll
+from photo.models import FilmFormat, Manufacturer, Film, Developer, FilmRoll, \
+    PhotoPaper, PhotoPaperFinish
+
 
 class FilmRollAdmin(admin.ModelAdmin):
     list_display = ('name', 'film', 'format', 'shot_date',)
@@ -18,3 +20,5 @@ admin.site.register(Manufacturer)
 admin.site.register(Film)
 admin.site.register(Developer)
 admin.site.register(FilmRoll, FilmRollAdmin)
+admin.site.register(PhotoPaper)
+admin.site.register(PhotoPaperFinish)
--- src/photo/models.py
+++ src/photo/models.py
@@ -1,5 +1,7 @@
-from django.db import models
 from django.contrib import auth
+from django.core.exceptions import ValidationError
+from django.db import models
+
 
 # Create your models here.
 class FilmFormat(models.Model):
@@ -51,4 +53,47 @@ class FilmRoll(models.Model):
 
     def _base_speed(self):
         return self.film.speed
+
+class PhotoPaperFinish(models.Model):
+    name = models.CharField(max_length=50);
+    
+    def __str__(self):
+        return self.name
+    
+class PhotoPaper(models.Model):
+    PAPER_TYPE_CHOICES = (
+        ('RC', "resin-coated"),
+        ('FB', "fibre-based",)
+    )
+    
+    GRADE_CHOICES = (
+        (-1, "00"),
+        (0, "0"),
+        (1, "1"),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5"))
+    
+    name = models.CharField(max_length=50)
+    manufacturer = models.ForeignKey(Manufacturer)
+    paper_type = models.CharField(max_length=2, choices=PAPER_TYPE_CHOICES)
+    multigrade = models.BooleanField()
+    grade = models.IntegerField(choices=GRADE_CHOICES, blank=True, null=True)
+    finishes = models.ManyToManyField(PhotoPaperFinish)
+    
+    def clean(self):
+        if self.multigrade is True:
+            if self.grade is not None:
+                raise ValidationError("Multigrade papers can not have a "
+                                      " grade specified")
+        else:
+            if self.grade is None:
+                raise ValidationError("Graded papers must have a grade "
+                                      "specified")
+                
+    def __str__(self):
+        return "{} {}".format(self.manufacturer.short_name, self.name)
+
+    
     
