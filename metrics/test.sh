ccf765b18452bb8cf0efb8fdfb7c62aad845185a
Rob Hailman <robhailman@gmail.com>
2016-03-31 15:26:41 -0400

    Add stop/time conversion to photo.utils
    These will eventually be needed for tracking burns/dodges to prints.

--- metrics/test.sh
+++ metrics/test.sh
@@ -2,3 +2,4 @@
 cd $VIRTUAL_ENV/src
 coverage run --branch --source='.' manage.py test
 coverage report > $VIRTUAL_ENV/metrics/coverage.txt
+coverage html -d $VIRTUAL_ENV/metrics/coverage
--- src/photo/utils.py
+++ src/photo/utils.py
@@ -2,9 +2,12 @@
 Utility classes and functions for the photo application
 '''
 
+import math
 import os
 import uuid
+
 from django.utils.deconstruct import deconstructible
+
 
 @deconstructible
 # pylint: disable=too-few-public-methods
@@ -34,3 +37,88 @@ class UploadToPathAndRename(object):
             filename = '{}.{}'.format(uuid.uuid4().hex, ext)
         # return the whole path to the file
         return os.path.join(self.sub_path, filename)
+
+class StopTimeConversion(object):
+    """Class for time and stop conversion methods."""
+
+    @staticmethod
+    def time_difference_in_stops(time_1, time_2):
+        """
+        Convert a difference in expose times to stops.
+
+        Args:
+            time_1: the base time
+            time_2: the new time
+        """
+
+        if time_1 <= 0 or time_2 <= 0:
+            raise ValueError("Times must be greater than 0.")
+
+        ratio = time_2 / time_1
+        return math.log2(ratio)
+
+    @staticmethod
+    def time_difference_in_points(time_1, time_2):
+        """
+        Convert a difference in exposure times to printer points.
+        A printer's point is 1/12 of a stop.
+
+        Args:
+            time_1: the base time
+            time_2: the new time
+        """
+
+        return StopTimeConversion.time_difference_in_stops(
+            time_1, time_2) * 12
+
+    @staticmethod
+    def stop_difference_to_multiplier(stops):
+        """
+        Convert a stop difference to a time multiplier.
+
+        Args:
+            stops: the difference in stops.
+        """
+
+        return 2 ** stops
+
+    @staticmethod
+    def point_difference_to_multiplier(points):
+        """
+        Convert a points difference to a time multiplier.
+
+        Args:
+            points: the difference in printer's points.
+        """
+        return StopTimeConversion.stop_difference_to_multiplier(points / 12)
+
+    @staticmethod
+    def adjust_time_by_stops(base_time, stops):
+        """
+        Adjust a given exposure time by a difference in stops.
+
+        Args:
+            base_time: the base exposure time to modify.
+            stops: the change in exposure, in stops.
+        """
+        if base_time <= 0:
+            raise ValueError("Base time must be greater than 0.")
+
+        multiplier = StopTimeConversion.stop_difference_to_multiplier(stops)
+        return base_time * multiplier
+
+    @staticmethod
+    def adjust_time_by_points(base_time, points):
+        """
+        Adjust a given exposure time by a difference in points.
+
+        Args:
+            base_time: the base exposure time to modify.
+            points: the change in exposure, in points.
+        """
+        if base_time <= 0:
+            raise ValueError("Base time must be greater than 0.")
+
+        multiplier = StopTimeConversion.point_difference_to_multiplier(
+            points)
+        return base_time * multiplier
