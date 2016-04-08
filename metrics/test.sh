fd9e3ff384554c5bc1e3b024d13ca40f50e049e5
Rob Hailman <robhailman@gmail.com>
2016-04-08 12:23:18 -0400

    Fix merge conflicts from pervious commit
    It would be helpful to really know how git works, but this should undo
    the mess from the merge commit.

--- metrics/test.sh
+++ metrics/test.sh
@@ -2,8 +2,4 @@
 cd $VIRTUAL_ENV/src
 coverage run --branch --source='.' manage.py test
 coverage report > $VIRTUAL_ENV/metrics/coverage.txt
-<<<<<<< HEAD
 coverage html -d $VIRTUAL_ENV/metrics/coverage
-=======
-coverage html -d $VIRTUAL_ENV/metrics/coverage
->>>>>>> e09ab69b9b6f9c55cdf7daaee820454ad15e7c07
